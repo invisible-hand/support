be97292a491c045d4a9bbf24df72f941d465246e
Andrey Zagoruiko <2500mbytes@gmail.com>
2022-09-01 23:31:57 -0700

    final commit + some changes for production 3

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,38 @@
-const path = require("path")
-const express = require("express")
-const dotenv = require("dotenv").config()
-const {errorHandler} = require('./middleware/errorMiddleware')
-const colors = require("colors")
-const connectDB = require("./config/db")
-const PORT = process.env.PORT || 5001
+const path = require('path')
+const express = require('express')
+require('colors')
+require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
 
-// connect to database
+// Connect to database
 connectDB()
 
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({extended: false }))
-
-app.get("/", (req, res) => {
-    res.send("hello")
-})
+app.use(express.urlencoded({ extended: false }))
 
 // Routes
-app.use("/api/users", require("./routes/userRoutes"))
-app.use("/api/tickets", require("./routes/ticketRoutes"))
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/build")))
-    app.get("*", (_, res) => { res.sendFile(path.join(__dirname, "../frontent/build/index.html"))})
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+  })
 } else {
-    app.get("/", (req, res) => {
-        res.send("hello")
-    })
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+  })
 }
 
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log(`server started on ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
