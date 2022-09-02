62e689f416313c8f484322ac3b0e2ea8e7cde4d8
Andrey Zagoruiko <2500mbytes@gmail.com>
2022-09-01 23:29:17 -0700

    final commit + some changes for production 2

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/tickets", require("./routes/ticketRoutes"))
 // Serve frontend
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")))
-    app.get("*", (req, res) => res.sendFile(__dirname, "../", "frontent", "build", "index.html"))
+    app.get("*", (_, res) => { res.sendFile(path.join(__dirname, "../frontent/build/index.html"))})
 
 } else {
     app.get("/", (req, res) => {
